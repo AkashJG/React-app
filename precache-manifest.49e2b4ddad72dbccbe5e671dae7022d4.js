bbd0c749faefa12ce97e879001dc0bbcdd1eb8fe
AkashJG <akashtiwari.innthink@gmail.com>
2020-08-15 22:43:52 +0530

    Updates

---
+++ precache-manifest.49e2b4ddad72dbccbe5e671dae7022d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d60b14fa593d7dcec074b5f0df6f58",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "962cd3b13ee775dce086",
+    "url": "/React-app/static/css/main.bd106972.chunk.css"
+  },
+  {
+    "revision": "2021c26b097247279bd7",
+    "url": "/React-app/static/js/2.e0ecc7cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app/static/js/2.e0ecc7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962cd3b13ee775dce086",
+    "url": "/React-app/static/js/main.14afa7b3.chunk.js"
+  },
+  {
+    "revision": "1f446c8922b0fa342d5d",
+    "url": "/React-app/static/js/runtime-main.63e1f374.js"
+  }
+]);
